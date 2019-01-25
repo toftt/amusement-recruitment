1751a3d3c209f6285e892031912ea6c07a4a2172
Joachim Toft <c.joachim.toft@gmail.com>
2019-01-25 14:41:00 +0100

    try to release

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "$WEB_DOCKER_IMAGE_ID"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization: Bearer $HEROKU_PASSWORD"
