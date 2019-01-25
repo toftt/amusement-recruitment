e9b20147c0e66c84431a404bee576c5e36a0646b
Joachim Toft <c.joachim.toft@gmail.com>
2019-01-25 16:26:21 +0100

    push it to the limit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+docker images
+docker tag org.toft.recsystem/recsystem:0.0.1-SNAPSHOT registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+
 echo "$WEB_DOCKER_IMAGE_ID"
+
+export WEB_DOCKER_IMAGE_ID=`docker inspect registry.heroku.com/$HEROKU_APP_NAME/web --format={{.Id}}`
+ls
+chmod a+x ./release.sh
+
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
   -d '{
   "updates": [
