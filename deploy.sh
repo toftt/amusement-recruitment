00717b847f95bb653b0e764e34be28f813136f26
Joachim Toft <c.joachim.toft@gmail.com>
2019-01-26 10:45:39 +0100

    escape quotes instead of single quotes

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,19 @@ docker images
 docker tag org.toft.recsystem/recsystem:0.0.1-SNAPSHOT registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
 
+export WEB_DOCKER_IMAGE_ID=`docker inspect registry.heroku.com/$HEROKU_APP_NAME/web --format={{.Id}}`
+
 echo "$WEB_DOCKER_IMAGE_ID"
 
-export WEB_DOCKER_IMAGE_ID=`docker inspect registry.heroku.com/$HEROKU_APP_NAME/web --format={{.Id}}`
-ls
-chmod a+x ./release.sh
-
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
-  -d '{
-  "updates": [
+  -d "{
+  \"updates\": [
     {
-      "type": "web",
-      "docker_image": "$WEB_DOCKER_IMAGE_ID"
+      \"type\": \"web\",
+      \"docker_image\": \"$WEB_DOCKER_IMAGE_ID\"
     }
   ]
-}' \
+}" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-  -H "Authorization: Bearer $HEROKU_PASSWORD"
+  -H "Authorization: Bearer $HEROKU_API_KEY"
