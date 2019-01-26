b6359b48c1bb6150bd75ce2b07902a6cc266a177
Joachim Toft <c.joachim.toft@gmail.com>
2019-01-26 10:57:17 +0100

    lets go boys

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
 }" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
-  -H "Authorization: Bearer $HEROKU_API_KEY"
+  -H "Authorization: Bearer $HEROKU_PASSWORD"
