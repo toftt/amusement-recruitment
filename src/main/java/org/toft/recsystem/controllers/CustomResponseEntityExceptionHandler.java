3998b9f479daed0d5b4b6164a5a204718537b445
Joachim Toft <c.joachim.toft@gmail.com>
2019-02-27 15:39:04 +0100

    files ** (this is intelliJ's fault, i swear)

---
+++ src/main/java/org/toft/recsystem/controllers/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,40 @@
+package org.toft.recsystem.controllers;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.toft.recsystem.domain.ApiError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        for (FieldError error: ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, "Validation failed.", errors);
+        return handleExceptionInternal(
+                ex, apiError, headers, apiError.getStatus(), request);
+
+    }
+}
